390da68756d214942abdfb3869c6ad6632c590a7
Cristian Quintero <cristian.quinterom94@hotmail.com>
2022-07-02 19:49:28 -0500

    Curso MINTIC java

---
+++ src/main/java/com/mycompany/cuotaprestamogui/CuotaPrestamoGUI.java
@@ -0,0 +1,15 @@
+package com.mycompany.cuotaprestamogui;
+
+/**
+ *
+ * @author Cristian Quintero
+ */
+public class CuotaPrestamoGUI {
+
+    public static void main(String[] args) {
+        
+        //Se instancia la ventana FrmCuotaPrestamo
+        FrmCuotaPrestamo frm = new FrmCuotaPrestamo();
+        frm.setVisible(true);
+    }
+}
